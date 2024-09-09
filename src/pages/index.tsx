a3dced4f5c51f6951353359a697ea8481c209c96
jollygrin <JollyGrin@protonmail.com>
2024-09-09 13:15:03 +0200

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <Grid h="100vh" w="100vw" placeItems="center">
       <img src="/cards/35/abundance-f.webp" />
+      <code>/cards/35/abundance-f.webp</code>
     </Grid>
   );
 }
