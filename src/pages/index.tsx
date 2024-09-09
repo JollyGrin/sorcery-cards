410d5a200d7be7ce8de72a1f4028d9c4cdeeae5a
jollygrin <JollyGrin@protonmail.com>
2024-09-09 13:41:44 +0200

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
-import { Grid } from "styled-system/jsx";
+import { Box, Grid, HStack } from "styled-system/jsx";
+
+const images = ["abundance-f.webp"];
 
 export default function Home() {
   return (
     <Grid h="100vh" w="100vw" placeItems="center">
-      <img src="/cards/35/abundance-f.webp" />
-      <code>/cards/35/abundance-f.webp</code>
+      <HStack>
+        {["", "50/", "20/", "5/"].map((e) => (
+          <Box key={e + "card"}>
+            <img src={`/cards/${e}abundance-f.webp `} />
+            <code
+              style={{
+                fontSize: "10px",
+                background: "rgba(0,0,0,0.1)",
+                padding: "5px 10px",
+                borderRadius: "4px",
+              }}
+            >
+              /cards/<strong>{e}</strong>abundance-f.webp
+            </code>
+          </Box>
+        ))}
+      </HStack>
     </Grid>
   );
 }
